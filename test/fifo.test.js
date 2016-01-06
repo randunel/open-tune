d9711e32d2762a9ccbf840dd74dffe9b408f6128
Mihai Ene <dkrandu@yahoo.com>
2016-01-06 18:52:20 +0000

    Clean up tmp file.

--- test/fifo.test.js
+++ test/fifo.test.js
@@ -4,21 +4,28 @@ var should = require('should');
 var createFifo = require('../lib/fifo.js');
 var util = require('../lib/util.js');
 
-describe('fifo', function() {
+describe('fifo', () => {
+    it('should cleanup', () => {
+        let fifo = createFifo({
+            dir: '/tmp',
+            name: 'test-fifo'
+        });
+        fifo.init().then(() => util.exec(fifo.getCallerPath()));
+        return fifo.wait()
+        .then(() => util.exec('ls /tmp'))
+        .then(list => list.should.not.containEql('test-fifo.sh'));
+    });
 
-    it('should work', function() {
+    it('should work', () => {
         let fifo = createFifo({
             dir: '/tmp',
             name: 'test-fifo'
         });
         let opts = {env: {hello: 'world'}};
         fifo.init().then(() => util.exec(fifo.getCallerPath(), opts));
-        return fifo.wait()
-            .then(data => {
-                should.exist(data);
-                data.hello.should.equal('world');
-            });
+        return fifo.wait().then(
+            data => should.exist(data.hello) && data.hello.should.equal('world')
+        );
     });
-
 });
 
