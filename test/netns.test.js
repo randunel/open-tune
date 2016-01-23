b026e0039a6927fe6c05ae337cc035122e0dbf46
Mihai Ene <dkrandu@yahoo.com>
2016-01-23 17:05:19 +0000

    Work with slower kernels.

--- test/netns.test.js
+++ test/netns.test.js
@@ -3,107 +3,109 @@
 let netns = require('../lib/netns');
 let util = require('../lib/util');
 
-describe('getUnusedNNS', () => {
-    describe('in clean state', () => {
+describe('network namespaces', () => {
+    describe('getUnusedNNS', () => {
+        describe('in clean state', () => {
+            before(() => util.exec('node dev/cleanup.js'));
+
+            it('should start counter with 0', () => netns.getUnusedNNS().then(
+                nns => {
+                    nns.name.should.equal('ot0');
+                }
+            ));
+
+            it('should start network pool with .1.0', () => netns.getUnusedNNS().then(
+                nns => {
+                    nns.network.should.equal('169.254.1.0');
+                }
+            ));
+        });
+
+        describe('when one network exists', () => {
+            before(() => netns.getUnusedNNS().then(netns.setupNNS));
+            after(() => util.exec('node dev/cleanup.js'));
+
+            it('should continue counter with 1', () => netns.getUnusedNNS().then(
+                nns => {
+                    nns.name.should.equal('ot1');
+                }
+            ));
+
+            it('should continue network pool with .1.4', () => netns.getUnusedNNS().then(
+                nns => {
+                    nns.network.should.equal('169.254.1.4');
+                }
+            ));
+        });
+
+        describe('when 2 networks exists', () => {
+            before(() => netns.getUnusedNNS()
+                .then(netns.setupNNS)
+                .then(netns.getUnusedNNS)
+                .then(netns.setupNNS));
+            after(() => util.exec('node dev/cleanup.js'));
+
+            it('should continue counter with 2', () => netns.getUnusedNNS().then(
+                nns => {
+                    nns.name.should.equal('ot2');
+                }
+            ));
+
+            it('should continue network pool with .1.8', () => netns.getUnusedNNS().then(
+                nns => {
+                    nns.network.should.equal('169.254.1.8');
+                }
+            ));
+        });
+    });
+
+    describe('destroy', () => {
+        afterEach(() => util.exec('node dev/cleanup.js'));
+
+        it('should delete a network namespace', () => netns.setupNNS(getNNSParams()).then(
+            () => netns.destroy(getNNSParams())
+        ).then(
+            () => util.exec(`ip netns list`)
+        ).then(
+            list => list.should.not.containEql('ot99')
+        ));
+
+        it('should delete netns rules in default ns', () => netns.setupNNS(getNNSParams()).then(
+            () => netns.destroy(getNNSParams())
+        ).then(
+            () => util.exec(`ip route show`)
+        ).then(
+            list => list.should.not.containEql('169.254')
+        ));
+
+        it('should delete netns iptables in default ns', () => netns.setupNNS(getNNSParams()).then(
+            () => netns.destroy(getNNSParams())
+        ).then(
+            () => util.exec(`iptables -t nat -vnL`)
+        ).then(
+            list => list.should.not.containEql('169.254')
+        ));
+    });
+
+    describe('setupNNS', () => {
         before(() => util.exec('node dev/cleanup.js'));
+        afterEach(() => util.exec('node dev/cleanup.js'));
 
-        it('should start counter with 0', () => netns.getUnusedNNS().then(
-            nns => {
-                nns.name.should.equal('ot0');
-            }
+        it('should create a network namespace', () => netns.setupNNS(getNNSParams()).then(
+            () => util.exec('ip netns show').then(list => {
+                list.should.containEql('ot99');
+            })
         ));
 
-        it('should start network pool with .1.0', () => netns.getUnusedNNS().then(
-            nns => {
-                nns.network.should.equal('169.254.1.0');
-            }
-        ));
-    });
-
-    describe('when one network exists', () => {
-        before(() => netns.getUnusedNNS().then(netns.setupNNS));
-        after(() => util.exec('node dev/cleanup.js'));
-
-        it('should continue counter with 1', () => netns.getUnusedNNS().then(
-            nns => {
-                nns.name.should.equal('ot1');
-            }
-        ));
-
-        it('should continue network pool with .1.4', () => netns.getUnusedNNS().then(
-            nns => {
-                nns.network.should.equal('169.254.1.4');
-            }
-        ));
-    });
-
-    describe('when 2 networks exists', () => {
-        before(() => netns.getUnusedNNS()
-            .then(netns.setupNNS)
-            .then(netns.getUnusedNNS)
-            .then(netns.setupNNS));
-        after(() => util.exec('node dev/cleanup.js'));
-
-        it('should continue counter with 2', () => netns.getUnusedNNS().then(
-            nns => {
-                nns.name.should.equal('ot2');
-            }
-        ));
-
-        it('should continue network pool with .1.8', () => netns.getUnusedNNS().then(
-            nns => {
-                nns.network.should.equal('169.254.1.8');
-            }
-        ));
-    });
-});
-
-describe('destroy', () => {
-    afterEach(() => util.exec('node dev/cleanup.js'));
-
-    it('should delete a network namespace', () => netns.setupNNS(getNNSParams()).then(
-        () => netns.destroy(getNNSParams())
-    ).then(
-        () => util.exec(`ip netns list`)
-    ).then(
-        list => list.should.not.containEql('ot99')
-    ));
-
-    it('should delete netns rules in default ns', () => netns.setupNNS(getNNSParams()).then(
-        () => netns.destroy(getNNSParams())
-    ).then(
-        () => util.exec(`ip route show`)
-    ).then(
-        list => list.should.not.containEql('169.254')
-    ));
-
-    it('should delete netns iptables in default ns', () => netns.setupNNS(getNNSParams()).then(
-        () => netns.destroy(getNNSParams())
-    ).then(
-        () => util.exec(`iptables -t nat -vnL`)
-    ).then(
-        list => list.should.not.containEql('169.254')
-    ));
-});
-
-describe('setupNNS', () => {
-    before(() => util.exec('node dev/cleanup.js'));
-    afterEach(() => util.exec('node dev/cleanup.js'));
-
-    it('should create a network namespace', () => netns.setupNNS(getNNSParams()).then(
-        () => util.exec('ip netns show').then(list => {
-            list.should.containEql('ot99');
-        })
-    ));
-
-    describe('network namespace', () => {
-        it('should access physical interface', () => netns.setupNNS(getNNSParams()).then(
-            () => util.exec('ip route get 8.8.8.8')
-        ).then(
-            output => util.exec(`ip netns exec ot99 ping -c 1 ${output.split('via ')[1].split(' ')[0]}`)
-        ).then(
-            output => output.should.match(/1\s(packets |)received/)
-        ));
+        describe('network namespace', () => {
+            it('should access physical interface', () => netns.setupNNS(getNNSParams()).then(
+                () => util.exec('ip route get 8.8.8.8')
+            ).then(
+                output => util.exec(`ip netns exec ot99 ping -c 1 ${output.split('via ')[1].split(' ')[0]}`)
+            ).then(
+                output => output.should.match(/1\s(packets |)received/)
+            ));
+        });
     });
 });
 
