e4b05b03ce8736f9973cd58438387e5b2e910d96
Mihai Ene <dkrandu@yahoo.com>
2016-01-27 00:12:53 +0000

    Coding style.

--- test/openvpn.test.js
+++ test/openvpn.test.js
@@ -12,23 +12,19 @@ describe('openvpn client', function() {
 
         it('should set up a working connection', () => client(getConfigPath())
             .then(ot => util.exec(`ip netns exec ${ot.nns.config.name} ping -c 1 8.8.8.8`))
-            .then(res => res.should.containEql('1 packet'))
-        );
+            .then(res => res.should.containEql('1 packet')));
 
         it('should allow default nns to use new connection ICMP', () => client(getConfigPath())
             .then(ot => util.exec(`traceroute -i ${ot.nns.config.vethDefault} -n -w 1 -I 8.8.8.8`))
-            .then(res => res.should.containEql(' ms'))
-        );
+            .then(res => res.should.containEql(' ms')));
 
         it('should allow default nns to use new connection UDP', () => client(getConfigPath())
             .then(ot => util.exec(`traceroute -i ${ot.nns.config.vethDefault} -n -w 1 -U 8.8.8.8`))
-            .then(res => res.should.containEql(' ms'))
-        );
+            .then(res => res.should.containEql(' ms')));
 
         it('should allow default nns to use new connection TCP', () => client(getConfigPath())
             .then(ot => util.exec(`traceroute -i ${ot.nns.config.vethDefault} -n -w 1 -T 8.8.8.8`))
-            .then(res => res.should.containEql(' ms'))
-        );
+            .then(res => res.should.containEql(' ms')));
     });
 
     describe('destroy', () => {
@@ -37,16 +33,12 @@ describe('openvpn client', function() {
         it('should destroy the netns', () => client(getConfigPath())
             .then(ot => ot.destroy()
                 .then(() => util.exec(`ip netns list`))
-                .then(list => list.should.not.containEql(ot.nns.name))
-            )
-        );
+                .then(list => list.should.not.containEql(ot.nns.name))));
 
         it('should kill the openvpn process', () => client(getConfigPath())
             .then(ot => ot.destroy()
-            .then(() => util.exec(`ps aux`))
-            .then(list => list.should.not.containEql(ot.id))
-            )
-        );
+                .then(() => util.exec(`ps aux`))
+                .then(list => list.should.not.containEql(ot.id))));
     });
 });
 
